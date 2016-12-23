23190cd48974e2ea2b7b7770da80cf15c316a59b
Andreas Knees <andreas.knees@nikem.de>
2016-12-23 19:49:35 +0100

    initial

---
+++ src/h2bug/TestServlet.java
@@ -0,0 +1,64 @@
+package h2bug;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			DataSource ds = InitialContext.doLookup("java:comp/env/jdbc/h2bug");
+			Connection con = ds.getConnection();
+			con.setAutoCommit(false);
+
+			try (PreparedStatement ps = con.prepareStatement("select aufgabenliste_seq.nextval");
+					ResultSet rs = ps.executeQuery();) {
+				rs.next();
+				System.out.println("Generated ID: " + rs.getInt(1));
+			}
+			
+			con.commit();
+			con.setAutoCommit(true);
+			con.close();
+		} catch (NamingException | SQLException e) {
+			e.printStackTrace();
+		}
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
